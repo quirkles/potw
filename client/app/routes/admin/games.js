a0ee84df8c0b799d02fa4e0117c2b53e8a9b8187
quirkles <al.quirk@gmail.com>
2020-04-10 20:27:47 -0400

    Add button

--- client/app/routes/admin/games.js
+++ client/app/routes/admin/games.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default class AdminGamesRoute extends Route {
+  model() {
+    return {
+      existingGames: [],
+      newGame: {},
+    }
+  }
 }
